5e5a7853d7637fc91eca1521eb3691865b7b6c94
mesar-e <mserkanergul@gmail.com>
2026-05-22 17:33:47 +0300

    palindrome metodu eklendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        int original = number;
+        int reversed = 0;
+
+        while (number > 0) {
+            reversed = (reversed * 10) + (number % 10);
+            number /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        return false;
+    }
+
+    public static String numberToWords(int number) {
+        return "";
+    }
 }
